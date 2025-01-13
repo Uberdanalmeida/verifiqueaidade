20cbdc1ea74a5c206c84d4910725079e1a867761
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-01-12 21:23:25 -0300

    Fazendo alterações de cores no js

--- script.js
+++ script.js
@@ -13,22 +13,26 @@ function clique() {
 
         if(data > 0 && data < 11) {
             resu.innerHTML = `Detectamos uma criança de ${data} anos.`
-            pri.style.backgroundColor = 'blue'
+            resu.style.fontSize = '22px'
+            pri.style.backgroundColor = 'rgba(29, 157, 248, 0.349)'
         }
 
         else if(data >= 11 && data < 21) {
             resu.innerHTML = `Detectamos um jovem de ${data} anos.`
-            pri.style.backgroundColor = 'blue'
+            resu.style.fontSize = '22px'
+            pri.style.backgroundColor = 'rgba(29, 157, 248, 0.349)'
         }
 
         else if(data >= 21 && data < 60) {
             resu.innerHTML = `Detectamos um ${mf} de ${data} anos.`
-            pri.style.backgroundColor = 'blue'
+            resu.style.fontSize = '22px'
+            pri.style.backgroundColor = 'rgba(29, 157, 248, 0.349)'
         }
 
         else {
             resu.innerHTML = `Detectamos um idoso de ${data} anos.`
-            pri.style.backgroundColor = 'blue'
+            resu.style.fontSize = '22px'
+            pri.style.backgroundColor = 'rgba(29, 157, 248, 0.349)'
         }
 
     }
@@ -38,21 +42,25 @@ function clique() {
 
         if(data > 0 && data < 11) {
             resu.innerHTML = `Detectamos uma criança de ${data} anos.`
+            resu.style.fontSize = '22px'
             pri.style.backgroundColor = 'pink'
         }
 
         else if(data >= 11 && data < 21) {
             resu.innerHTML = `Detectamos uma jovem de ${data} anos.`
+            resu.style.fontSize = '22px'
             pri.style.backgroundColor = 'pink'
         }
 
         else if(data >= 21 && data < 60) {
             resu.innerHTML = `Detectamos uma ${mf} de ${data} anos.`
+            resu.style.fontSize = '22px'
             pri.style.backgroundColor = 'pink'
         }
 
         else {
             resu.innerHTML = `Detectamos uma idosa de ${data} anos.`
+            resu.style.fontSize = '22px'
             pri.style.backgroundColor = 'pink'
         }
 
